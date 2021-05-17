279389a63430337561d6c2218af3b5e30df17732
Jorge De La Isla <55961839+Jorge1-web@users.noreply.github.com>
2021-05-17 18:51:54 -0500

    INE proyecto
    incluyen los archivos del avance del proyecto.

---
+++ INE proyecto/sesion.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <title>reservas</title>
+   <meta name="viewport" content="whidth=device-whidth, user-scalable=no,initial-scale=1.0,maximun-scale=1.0, minimun-scale=1.0"></head>
+   <link rel="stylesheet" href="css/reservas.css">
+   <link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+<header class="header">
+      
+</header> 
+     
+</body>
+</html>
